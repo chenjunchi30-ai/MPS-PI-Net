e0728b63fdce2769787fa7b51fa6365a237c7d28
chenjunchi30-ai <cjc0918@mail.dlut.edu.cn>
2026-02-01 15:56:31 +0800

    Initial commit

--- models/spynet_arch.py
+++ models/spynet_arch.py
@@ -9,16 +9,16 @@ from .arch_util import flow_warp
 class BasicModule(nn.Module):
     """Basic Module for SpyNet.
     """
-
+Z
     def __init__(self):
         super(BasicModule, self).__init__()
 
         self.basic_module = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=32, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=16, out_channels=2, kernel_size=7, stride=1, padding=3))
+            nn.Conv2d(in_channels=2, out_channels=8, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
+            nn.Conv2d(in_channels=8, out_channels=4, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
+            nn.Conv2d(in_channels=4, out_channels=2, kernel_size=7, stride=1, padding=3))
 
     def forward(self, tensor_input):
         return self.basic_module(tensor_input)
